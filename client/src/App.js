27dd0490cc276e4d3b64f90d35efa75e939462af
Jim Doan <jimdoan4@gmail.com>
2019-04-15 12:33:28 -0400

    created components folder and added local host 3001

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
