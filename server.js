648deaa52a67b7e77abf1e3c4434d1b653f4736b
Jim Doan <jimdoan4@gmail.com>
2019-04-17 15:39:05 -0400

    deleted some dependencies

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('rootpath')();
+// require('rootpath')();
 const express = require('express');
 const logger = require('morgan');
 const app = express();
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/client/build'));
 
 app.use('/api/', routes);
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/client/build/index.html');
 });
 const PORT = process.env.PORT || 3001;
@@ -21,18 +21,3 @@ app.listen(PORT, () => {
 	console.log('App is up and running on port ' + PORT);
 });
 
-
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const jwt = require('_helpers/jwt');
-// const errorHandler = require('_helpers/error-handler');
-
-
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(cors());
-// // use JWT auth to secure the api
-// app.use(jwt());
-// // global error handler
-// app.use(errorHandler);
